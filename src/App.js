77f78cb9eb40e94f4d8777baf1d4d18a23d4edf9
Nova Blackstock <fractalrefletions@gmail.com>
2020-07-14 13:28:01 -0700

    React2 defeated by Nova

--- src/App.js
+++ src/App.js
@@ -5,16 +5,36 @@ Look at each file to see what props need to be passed.
 */
 
 // Import the state hook
-import React from "react";
+import React, {useState} from "react";
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
+import LikeSection from './components/Posts/LikeSection'
+import Post from './components/Posts/Post'
+import PostHeader from './components/Posts/PostHeader'
+import Posts from './components/Posts/Posts'
+
+import SearchBar from './components/SearchBar/SearchBar'
+
+import data from './dummy-data'
+
 import "./App.css";
 
 const App = () => {
   // Create a state called 'posts' to hold the list of posts, initializing to dummyData.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(data)
+  const [searchBar, setsearchBar] = useState(SearchBar)
 
   const likePost = postId => {
+    setPosts(
+      posts.map(post =>{
+        if(postId === post.id){
+          return {...post, likes: post.likes+1}
+        }else{
+          return post
+        }
+      })
+    )
     // This function is passed into nested components using props, to allow them to update application state.
     // It takes a post id as its only argument. The idea is to increase the 'likes' count of the post with the given `id`.
     // We will update the posts slice of state using `setPosts`, passing as the new state the invocation of `posts.map()`.
@@ -26,6 +46,8 @@ const App = () => {
   return (
     <div className="App">
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts likePost = {likePost} posts = {posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
